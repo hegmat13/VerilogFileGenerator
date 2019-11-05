581a58013502b12be6aecab2c028698a59f7a431
aalvarez11 <43628012+aalvarez11@users.noreply.github.com>
2019-11-05 11:21:28 -0700

    Ian's version

---
+++ input.hpp
@@ -0,0 +1,39 @@
+//
+//  input.hpp
+//  dpgen
+//
+//  Created by Ian Hooks on 10/29/19.
+//  Copyright © 2019 Ian Hooks. All rights reserved.
+//
+
+#ifndef input_hpp
+#define input_hpp
+
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "Inputs.hpp"
+#include "Outputs.hpp"
+#include "Wires.hpp"
+#include "Equations.hpp"
+
+class verilogSim {
+private:
+    std::vector<Inputs> _inputs;
+    std::vector<Outputs> _outputs;
+    std::vector<Wires> _wires;
+    std::vector<Equations> _equations;
+    char* _inputFileName;
+    char* _outputFileName;
+    
+public:
+    void run(char* _inputFileName, char* _outputFileName);
+    void ReadCommandsFromFile();
+
+};
+
+#endif /* input_hpp */
